f8d6f102400ce5582de7428bfb94546e42221f26
Anthony Barragan <anthony.bar.89@gmail.com>
2022-03-20 22:08:09 -0700

    added homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 'use strict';
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 // if we make a request to any api endpoint that doesn't exist, then they get a 404. 
 router.use((req, res) => {
--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 // db connection
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+// allow express to parse JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+// make css static files
+app.use(express.static(path.join(__dirname, 'public')));
 // Turn on routes
 app.use(routes);
-
+// set up handlebars connection
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars')
 // turn on connection to db and server
 sequelize.sync({ force: false })
     .then(() => {
