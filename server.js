2e7b5787108733a4738abe59c279d037a0b93523
Anthony Barragan <anthony.bar.89@gmail.com>
2022-03-23 21:11:24 -0700

    force to true

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(routes);
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars')
 // turn on connection to db and server
-sequelize.sync({ force: false })
+sequelize.sync({ force: true })
     .then(() => {
         app.listen(PORT, () => console.log('Now listening'));
     });
